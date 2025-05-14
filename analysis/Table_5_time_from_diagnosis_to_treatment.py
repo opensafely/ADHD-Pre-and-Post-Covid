3d4f6d9e67f6eccb49e893f220ffd9e1da68e7b9
Kin Quan <kenneth.quan1@nhs.net>
2025-05-14 11:01:22 +0000

    adding changes

--- analysis/Table_5_time_from_diagnosis_to_treatment.py
+++ analysis/Table_5_time_from_diagnosis_to_treatment.py
@@ -14,10 +14,18 @@ os.makedirs(output_dir, exist_ok=True)
 adhd_medication_date_data = pd.read_csv("output/Patient_table_5_dia_to_med.csv.gz")
 
 #Genrating the final table
-output = adhd_medication_date_data.groupby('year_of_medication').times_between_dia_med_weeks.agg(['mean', 'median'])
+output = adhd_medication_date_data.groupby(['year_of_medication','sex']).times_between_dia_med_weeks.agg(['mean', 'median','size'])
 
-#Adding a set time stamp
-output['timestamp'] = add_datestamp()
+# #Filter the muilple index
+output = output[output.index.isin(list(range(2016,2025)), level=0)]
+
+# #Adding a small number suprresion
+rounding_unit = 10
+output['size'] = np.ceil(output['size'] / rounding_unit)
+output['size'] = output['size'] * rounding_unit
+
+# #Adding a set time stamp
+ output['timestamp'] = add_datestamp()
 
 # Saving the table
 output.to_csv("output/Table_5_time_from_diagnosis_to_treatment.csv")
