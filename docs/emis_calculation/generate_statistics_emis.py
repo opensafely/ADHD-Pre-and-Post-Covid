b7f39e1144f1e51424d9fe1fdf082168cc1eb671
Kin Quan <kenneth.quan1@nhs.net>
2025-07-24 16:23:12 +0000

    getting the adhd counts

--- docs/emis_calculation/generate_statistics_emis.py
+++ docs/emis_calculation/generate_statistics_emis.py
@@ -2,21 +2,25 @@ import pandas as pd
 import requests
 
 url = "https://files.digital.nhs.uk/FC/187D35/health_care_ld_sicbl_2023-24.csv"
+file_path = "docs/emis_calculation/LD_data.csv"
+groupby_index = ['YEAR','SEX','AGE_BAND']
+adhd_code = ['LDOB089','LDOB091']
+
+
 df_emis = pd.read_csv(url)
 
 response = requests.get(url)
 
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    # Save the content of the response to a local CSV file
-    with open("docs/emis_calculation/LD_data.csv", "wb") as f:
-        f.write(response.content)
-
-else:
-    print("Failed to download CSV file. Status code:", response.status_code)
+with open(file_path, "wb") as f:
+    f.write(response.content)
 
 # Read the CSV file into a Pandas DataFrame
-df = pd.read_csv("LD_data.csv")
+ld_data_adhd = pd.read_csv(file_path)
 
 # Need to filter the codes LDOB089 and LDOB091
-adhd_counts = df_emis
+adhd_counts = ld_data_adhd[ld_data_adhd['INDICATOR'].isin(adhd_code)]
+
+#Need to sum over
+adhd_counts = adhd_counts.groupby(groupby_index, as_index=False)['VALUE'].sum()
+
+adhd_counts
