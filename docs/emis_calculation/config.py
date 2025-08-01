be3aa288b9257194c2327974dbaae06208b6a411
Kin Quan <kenneth.quan1@nhs.net>
2025-08-01 16:12:10 +0000

    saving files

--- docs/emis_calculation/config.py
+++ docs/emis_calculation/config.py
@@ -3,17 +3,12 @@
 config = {
     'url_2324':"https://files.digital.nhs.uk/FC/187D35/health_care_ld_sicbl_2023-24.csv",
     'url_2223':"https://files.digital.nhs.uk/43/BD15AC/health_care_ld_sicbl_2022-23.csv",
+    'url_1722':"https://files.digital.nhs.uk/49/70A43E/health_care_ld_ccg_data_1617_2021.zip",
+    'url_1621':"https://files.digital.nhs.uk/49/70A43E/health_care_ld_ccg_data_1617_2021.zip",
+    'file_path_to_save':"docs/emis_calculation/",
+    "groupby_index":['YEAR','SEX','AGE_BAND'],
+    'numerator' : ['LDOB089','LDOB091'],
+    'denominator' : ['LDOB003A' , 'LDOB003B'],
+    'all_code' : ['ALL'],
+    'sex_rename' : {'F':'female' , 'M':'male'}
     }
-
-url = "https://files.digital.nhs.uk/FC/187D35/health_care_ld_sicbl_2023-24.csv"
-file_path = "docs/emis_calculation/LD_data.csv"
-
-groupby_index = ['YEAR','SEX','AGE_BAND']
-
-adhd_code = ['LDOB089','LDOB091']
-
-deoinator = ['LDOB003A' , 'LDOB003B']
-
-all_code = ['ALL']
-
-sex_rename = {'F':'female' , 'M':'male'}
--- docs/emis_calculation/generate_statistics_emis.py
+++ docs/emis_calculation/generate_statistics_emis.py
@@ -1,29 +1,41 @@
 import pandas as pd
 import requests
 from config import config
+import os
+
+# Need to save the data
+keys_in_config_dict = list(config.keys())
+url_keys = [x for x in keys_in_config_dict if ('url' in x)]
+
+for each_url_key in url_keys:
+    url = config[each_url_key]
+    response = requests.get(url)
+    filename = config['file_path_to_save'] + os.path.basename(url)
+    with open(filename, "wb") as f:
+        f.write(response.content)
 
 
-df_emis = pd.read_csv(url)
+# df_emis = pd.read_csv(url)
 
-response = requests.get(url)
+# response = requests.get(url)
 
-with open(file_path, "wb") as f:
-    f.write(response.content)
+# with open(file_path, "wb") as f:
+#     f.write(response.content)
 
-# Read the CSV file into a Pandas DataFrame
-ld_data_adhd = pd.read_csv(file_path)
-ld_data_adhd['SEX'] = ld_data_adhd['SEX'].replace(sex_rename)
+# # Read the CSV file into a Pandas DataFrame
+# ld_data_adhd = pd.read_csv(file_path)
+# ld_data_adhd['SEX'] = ld_data_adhd['SEX'].replace(sex_rename)
 
-# Need to filter the codes LDOB089 and LDOB091
-adhd_counts = ld_data_adhd[ld_data_adhd['INDICATOR'].isin(adhd_code)]
-all_counts = ld_data_adhd[ld_data_adhd['INDICATOR'].isin(all_code)]
+# # Need to filter the codes LDOB089 and LDOB091
+# adhd_counts = ld_data_adhd[ld_data_adhd['INDICATOR'].isin(adhd_code)]
+# all_counts = ld_data_adhd[ld_data_adhd['INDICATOR'].isin(all_code)]
 
-#Need to sum over
-adhd_counts = adhd_counts.groupby(groupby_index, as_index=False)['VALUE'].sum()
-all_counts = all_counts.groupby(groupby_index, as_index=False)['VALUE'].sum()
+# #Need to sum over
+# adhd_counts = adhd_counts.groupby(groupby_index, as_index=False)['VALUE'].sum()
+# all_counts = all_counts.groupby(groupby_index, as_index=False)['VALUE'].sum()
 
-#Need to some renaming
-adhd_counts = adhd_counts.rename(columns={'VALUE': 'numerator'})
-all_counts = all_counts.rename(columns={'VALUE': 'denominator'})
+# #Need to some renaming
+# adhd_counts = adhd_counts.rename(columns={'VALUE': 'numerator'})
+# all_counts = all_counts.rename(columns={'VALUE': 'denominator'})
 
-print(all_counts)
+# print(all_counts)
