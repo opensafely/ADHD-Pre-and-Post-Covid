61a0cd992bab3cbbb0a9bdfd97b678e0fa68b792
Kin Quan <kenneth.quan1@nhs.net>
2025-08-04 13:53:56 +0000

    saving the files

--- docs/emis_calculation/config.py
+++ docs/emis_calculation/config.py
@@ -1,10 +1,12 @@
 config = {
+    'saved_path':'emis_measure',
     'url_2324':"https://files.digital.nhs.uk/FC/187D35/health_care_ld_sicbl_2023-24.csv",
     'url_2223':"https://files.digital.nhs.uk/43/BD15AC/health_care_ld_sicbl_2022-23.csv",
     'url_1722':"https://files.digital.nhs.uk/BC/016738/health_care_ld_sicbl_data_1718_2122.zip",
     'url_1621':"https://files.digital.nhs.uk/49/70A43E/health_care_ld_ccg_data_1617_2021.zip",
     'main_link': "https://digital.nhs.uk/data-and-information/publications/statistical/health-and-care-of-people-with-learning-disabilities",
-    'file_path_to_save': "docs/emis_calculation/",
+    'file_path_to_save': "docs/emis_calculation/source_files/",
+    'file_path_to_emis_measure': "docs/emis_calculation/emis_measure.csv",
     'list_of_csv':{
         '23_24':'health_care_ld_sicbl_2023-24.csv',
         '22_23':'health_care_ld_sicbl_2022-23.csv',
--- docs/emis_calculation/generate_statistics_emis.py
+++ docs/emis_calculation/generate_statistics_emis.py
@@ -15,6 +15,8 @@ if missing_files:
 
 dict_of_files = config['list_of_csv']
 
+combined_table_emis = pd.DataFrame()
+
 for each_key in list(dict_of_files.keys()):
 
     #opening each file
@@ -23,22 +25,6 @@ for each_key in list(dict_of_files.keys()):
     #Wrangle the file to match OS's outputs
     each_ratio = utils.wrangling_table_to_opensafely_form(each_csv,each_key,config)
 
+    combined_table_emis = pd.concat([combined_table_emis,each_ratio], axis=0, ignore_index=True)
 
-
-# # Read the CSV file into a Pandas DataFrame
-# ld_data_adhd = pd.read_csv(file_path)
-# ld_data_adhd['SEX'] = ld_data_adhd['SEX'].replace(sex_rename)
-
-# # Need to filter the codes LDOB089 and LDOB091
-# adhd_counts = ld_data_adhd[ld_data_adhd['INDICATOR'].isin(adhd_code)]
-# all_counts = ld_data_adhd[ld_data_adhd['INDICATOR'].isin(all_code)]
-
-# #Need to sum over
-# adhd_counts = adhd_counts.groupby(groupby_index, as_index=False)['VALUE'].sum()
-# all_counts = all_counts.groupby(groupby_index, as_index=False)['VALUE'].sum()
-
-# #Need to some renaming
-# adhd_counts = adhd_counts.rename(columns={'VALUE': 'numerator'})
-# all_counts = all_counts.rename(columns={'VALUE': 'denominator'})
-
-# print(all_counts)
+combined_table_emis.to_csv(config['file_path_to_emis_measure'])
