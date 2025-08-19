3b3bb4ca033befafa690fe7171149bcd0ce50ec5
Kin Quan <kenneth.quan1@nhs.net>
2025-08-19 11:02:12 +0000

    remission

---
+++ docs/remission_calculation/config.py
@@ -0,0 +1,4 @@
+config = {
+    'saved_files_folder': 'docs/remission_calculation/source_files'
+    'SNOMED_code_usage_2023_24': 'https://files.digital.nhs.uk/70/C33DEC/SNOMED_code_usage_2023-24.xlsx'
+}
